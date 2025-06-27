5fd2e197ec55d19eb1876995a602431515acd6ea
canvrno <kevin@cline.bot>
2025-06-26 18:39:46 -0700

    test

--- test-setup.js
+++ test-setup.js
@@ -11,15 +11,42 @@ const tsConfig = JSON.parse(fs.readFileSync(path.join(baseUrl, "tsconfig.json"),
  * However, `tsc` doesn't compile paths by itself
  * (https://www.typescriptlang.org/docs/handbook/modules/reference.html#paths-does-not-affect-emit)
  * So we need to use tsconfig-paths to resolve the aliases when running tests,
- * but pointing to `out` instead.
+ * but pointing to `out/src` instead of just `out`.
  */
 const outPaths = {}
 Object.keys(tsConfig.compilerOptions.paths).forEach((key) => {
 	const value = tsConfig.compilerOptions.paths[key]
-	outPaths[key] = value.map((path) => path.replace("src", "out"))
+	outPaths[key] = value.map((path) => path.replace("src", "out/src"))
 })
 
 tsConfigPaths.register({
 	baseUrl: baseUrl,
 	paths: outPaths,
 })
+
+// Initialize host providers for integration tests
+const hostProviders = require("./out/src/hosts/host-providers")
+
+// Create mock host bridge client
+const mockHostBridgeClient = {
+	workspaceClient: {
+		getWorkspacePaths: async () => ({
+			id: undefined,
+			paths: ["/mock/workspace"],
+		}),
+	},
+	uriServiceClient: {
+		file: async () => ({}),
+		joinPath: async () => ({}),
+		parse: async () => ({}),
+	},
+	watchServiceClient: {
+		subscribeToFile: () => () => {},
+	},
+}
+
+// Create mock webview provider creator
+const mockWebviewProviderCreator = () => ({})
+
+// Initialize host providers with mocks
+hostProviders.initializeHostProviders(mockWebviewProviderCreator, mockHostBridgeClient)
